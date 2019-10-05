df6c4ac0a55ac21ebdfda348dc2358288c7d11a6
Hong Kwon <hongkwon.dev@gmail.com>
2019-10-05 23:51:06 +0200

    shopping cart add quantity select + style

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 class CartItem extends React.Component {
     render() {
         const { name, price, quantity, imageUrl } = this.props;
+        const quantityOptions = Array.from(Array(10), (e, i) => {
+            return <option key={i + 1} value={i + 1}>{i + 1}</option>
+        });
 
         return (
             <li className="cart-item">
-                <div className="product">
-                    <div className="product-image"><img src={imageUrl} alt={name} width="10rem" /></div>
-                    <div className="product-content">
-                        <h2 className="card-title">{name}</h2>
-                        <p className="carde-text text-ellipsis">{price} x {quantity}</p>
-                    </div>
-                </div>
+                <span className="cart-item-image">
+                    <img src={imageUrl} alt={name} />
+                </span>
+                <h2 className="cart-item-name">{name}</h2>
+                <p className="cart-item-price text-ellipsis">{price}</p>
+                <select className="cart-item-quantity" defaultValue={quantity}>
+                    {quantityOptions}
+                </select>
+                <button className="btn-remove-from-cart">
+                    <FontAwesomeIcon icon={faTrashAlt} size="lg" />
+                </button>
             </li>
         );
     }
--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CartList from '../CartList';
 import ProductDetail from '../ProductDetail';
